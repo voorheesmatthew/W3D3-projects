e4bd61c6e418db9110b3eee11ac8270f4cd48b0f
Matt <voorhees.matthew@gmail.com>
2018-09-05 12:16:41 -0700

    Complete setup for associations project

--- associations/app/models/enrollment.rb
+++ associations/app/models/enrollment.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint(8)        not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Enrollment < ApplicationRecord
 end
