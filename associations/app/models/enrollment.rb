47963c5b65e882c84a50003408dd3b2d6de81978
Matt <voorhees.matthew@gmail.com>
2018-09-05 14:16:03 -0700

    Complete Associations Project

--- associations/app/models/enrollment.rb
+++ associations/app/models/enrollment.rb
@@ -10,4 +10,13 @@
 #
 
 class Enrollment < ApplicationRecord
+  belongs_to :user,
+  primary_key: :id,
+  foreign_key: :student_id,
+  class_name: :User
+  
+  belongs_to :course, #Pointer A 
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Course
 end
